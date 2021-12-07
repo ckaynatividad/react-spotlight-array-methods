f02f8d54b7371e4cb4891390489b1ad0d7f404bf
ckaynatividad <natividad.ckay@gmail.com>
2021-12-07 14:58:52 -0800

    tests passing 1-5

--- index.js
+++ index.js
@@ -2,28 +2,41 @@
 // OUTPUT: an array of the names
 // REQS: use .map
 
-export const getCountryNames = (countries) => {};
+export const getCountryNames = (countries) => {
+  const countryNames = countries.map((countries) => countries.name);
+  return countryNames;
+};
 
 // INPUT: the array of countries from data.js
 // OUTPUT: an array of the names and their iso2 country codes using .map
 // REQS: use .map
 
-export const getCountryCodes = (countries) => {};
+export const getCountryCodes = (countries) => {
+  const countryCodes = countries.map((countries) => [countries.name, countries.iso2]);
+  return countryCodes;
+};
 
 // INPUT: the array of countries from data.js
 // OUTPUT: only countries in Oceana
 // REQS: use .filter
 
-export const filterOceania = (countries) => {};
+export const filterOceania = (countries) => {
+  return countries.filter((countries) => countries.continent === 'Oceania');
+};
 
 // INPUT: the array of countries from data.js
 // OUTPUT: only countries that include the letters 'au'
 // REQS: use .filter
 
-export const filterAu = (countries) => {};
+export const filterAu = (countries) => {
+  return countries.filter((countries) => countries.name.includes('au'));
+};
 
 // INPUT: the array of brands from data.js
 // OUTPUT: an array of all the sneakers brands
 // REQS: use .reduce
 
-export const getSneaks = (brands) => {};
+export const getSneaks = (brands) => {
+  const sneakerBrands = brands.reduce((acc, brand) => [...acc, ...brand.shoes], []);
+  return sneakerBrands;
+};
